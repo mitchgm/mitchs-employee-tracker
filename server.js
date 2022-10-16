cb3d94806d1d566b3795362ef645bacaee877d8c
mitchgm <mitchgm@bgsu.edu>
2022-10-16 12:34:49 -0700

    start starter prompts

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // required constants 
 const inquirer = require("inquirer");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const express = require("express");
 const table = require("console.table");
 
@@ -25,3 +25,28 @@ user: "root",
     database: "employees",
 });
 // end of connection function
+
+// this fucntion will connect the sql databse and the mysql together 
+connection.connect(function (err) {
+    if (err) throw err;
+    starterPro();
+    // this will call the intial prompts
+});
+
+function starterPro() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "task",
+            message: "Select a Task",
+            choices: [
+                "View The Employees",
+                "View The Departments",
+                "Add an Employee",
+                "Remove an Employees",
+                "Update an Employee Role",
+                "Add an Employee Role",
+                "Exit",
+            ],
+        })
+}
