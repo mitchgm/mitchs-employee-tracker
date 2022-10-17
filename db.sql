d281d3d5c9df92bf2de83323ffb776a8f2533fb4
mitchgm <mitchgm@bgsu.edu>
2022-10-16 18:59:12 -0700

    db sql touch ups

--- db.sql
+++ db.sql
@@ -1,10 +1,31 @@
 
-DROP TABLE IF EXISTS employee; 
-DROP TABLE IF EXISTS jobs;
-DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS employeeView; 
+DROP TABLE IF EXISTS roleView;
+DROP TABLE IF EXISTS departmentView;
 -- this resets if the use case is accessing other data
 
 
+CREATE TABLE departmentView (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(40) NOT NULL,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE roleView (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(40) NOT NULL,
+  salary DECIMAL(10.3) NOT NULL,
+  department_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employeeView (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id int ,
+  PRIMARY KEY (id)
+);
 
 
