8ccf00b822bacc50df5bc112920ab3b7cb5fd5ac
mitchgm <mitchgm@bgsu.edu>
2022-10-16 19:03:48 -0700

    touchup

--- db.sql
+++ db.sql
@@ -6,23 +6,29 @@ DROP TABLE IF EXISTS departmentView;
 
 
 CREATE TABLE departmentView (
+    -- sets the id as the primary key
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(40) NOT NULL,
+  -- sets character number
   PRIMARY KEY (id)
 );
 
 CREATE TABLE roleView (
+    -- sets the id as the primary key
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(40) NOT NULL,
   salary DECIMAL(10.3) NOT NULL,
+   -- sets character number
   department_id INT,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE employeeView (
+    -- sets the id as the primary key
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(40) NOT NULL,
   last_name VARCHAR(40) NOT NULL,
+   -- sets character number
   role_id INT NOT NULL,
   manager_id int ,
   PRIMARY KEY (id)
